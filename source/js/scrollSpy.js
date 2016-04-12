557c85d4d53123847ed4724994702e0c9e98309c
jon <jonathan.lazarini@wipro.com>
2016-04-12 17:13:34 +0100

    Refactor old js for nav, header  into a module, Add scrollspy to the website

--- source/js/scrollSpy.js
+++ source/js/scrollSpy.js
@@ -2,35 +2,48 @@
  * Created by jonlazarini on 04/04/16.
  */
 //TODO Load plugins from bower - https://github.com/janpaepke/ScrollMagic/wiki/Getting-Started-:-First-Steps
-
-import tweenMax from '../vendors/gsap/src/uncompressed/TweenMax.js';
 import $ from '../vendors/jquery/dist/jquery.js';
-//TODO fix what to import for scrollspy
-import tweenLite from '../vendors/gsap/src/uncompressed/TweenLite.js';
+import TweenMax from '../vendors/gsap/src/uncompressed/TweenMax.js';
 
-import scrollMagic from '../vendors/scrollmagic/scrollmagic/uncompressed/ScrollMagic.js';
+//TODO fix what to import for scrollspy
+//import TweenLite from '../vendors/gsap/src/uncompressed/TweenLite.js';
+
+import ScrollMagic from '../vendors/scrollmagic/scrollmagic/uncompressed/ScrollMagic.js';
+
+//Can't pass pluggins when there is many dots in the filename e.g. jquery.gsap.js
+//import Animation from '../vendors/scrollmagic/scrollmagic/uncompressed/plugins/gsap.js';
+//import Animation from '../vendors/scrollmagic/scrollmagic/uncompressed/plugins/jquery.gsap.js';
+import ScrollToPlugin from '../vendors/gsap/src/uncompressed/plugins/ScrollToPlugin.js';
+
+
+
 
 // Can't pass debugger...
 //import debugScroll from '../../vendors/scrollmagic/scrollmagic/uncompressed/plugins/debug.addIndicators.js';
 
 
 //Debug modules loading
-console.log('\n\n' + tweenMax + '\n\n');
-console.log('Log jquery fromn scrollSpy: \n\n'+ $ + '\n\n');
-console.log('\n\n' + tweenLite + '\n\n');
-console.log('\n\n' + scrollMagic + '\n\n');
+console.log('Log jquery from scrollSpy: \n\n'+ $ + '\n\n');
+console.log('TweenMax: \n\n' + TweenMax + '\n\n');
+
+//console.log('\n\n' + TweenLite + '\n\n');
+
+console.log('scrollMagic: \n\n' + ScrollMagic + '\n\n');
+
+//console.log('Animation: \n\n' + Animation + '\n\n');
+console.log('ScrollToPlugin: \n\n' + ScrollToPlugin + '\n\n');
 
 
 // test
-export default class TaMere {
-    constructor(fdp, tromblon) {
-        this.fdp = fdp;
-        this.tromblon = tromblon;
-    }
-}
-
-var taMere = new TaMere('jacky','michel');
-console.log(taMere);
+//export default class TaMere {
+//    constructor(fdp, tromblon) {
+//        this.fdp = fdp;
+//        this.tromblon = tromblon;
+//    }
+//}
+//
+//var taMere = new TaMere('jacky','michel');
+//console.log(taMere);
 
 // Can't pass debugger...
 //console.log('\n\n' + debugScroll + '\n\n');
@@ -41,6 +54,51 @@ console.log(taMere);
 // Require jquery
 
 //TODO Inject scrollspy.s file into functions.js
+
+// Init controller
+var controller = new ScrollMagic.Controller();
+
+// Change behavior of controller
+// to animate scroll instead of jump
+controller.scrollTo(function(target) {
+
+    TweenMax.to(window, 0.5, {
+        scrollTo : {
+            y : target, // scroll position of the target along y axis
+            autoKill : true // allows user to kill scroll action smoothly
+        },
+        ease : Cubic.easeInOut
+    });
+
+});
+
+//  Bind scroll to anchor links
+$(document).on("click", "a[href^=#]", function(e) {
+    var id = $(this).attr("href");
+
+    if($(id).length > 0) {
+        e.preventDefault();
+
+        // trigger scroll
+        controller.scrollTo(id);
+
+        // If supported by the browser we can also update the URL
+        if (window.history && window.history.pushState) {
+            history.pushState("", document.title, id);
+        }
+    }
+
+});
+
+
+
+
+
+
+
+
+
+
 
 //module.exports = function() {
 //
